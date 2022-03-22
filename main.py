30c2e01ad55d78d3eb5fbd4355babea0b6849770
arushi10 <55467799+arushi10@users.noreply.github.com>
2022-03-22 18:43:10 +0000

    week 2 file organization

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from subprocess import call
 
 # call(["python", "week0/week0_menu.py"])
 
-call(["python", "week1/week1_menu.py"])
+# call(["python", "week1/week1_menu.py"])
 
-# call(["python", "week1/infoDB_lists.py"])
-
-# call(["python", "week1/infoDB_loops.py"])
-
-# call(["python", "week1/fibonacci.py"])
+call(["python", "week2/week2_menu.py"])
